0639ee7117a3a661a0c1769472f671d6876a6827
Thomas Hunt <thomas.a.hunt.4@gmail.com>
2025-09-13 16:47:05 -0300

    django setup for study while I build

---
+++ jardin/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jardin project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jardin.settings')
+
+application = get_wsgi_application()
